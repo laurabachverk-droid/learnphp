9ffcc53f295f99140454164ef6cf21cac585dc38
laura Bachverk <laura.Bachverk@gmail.com>
2026-09-01 22:22:14 -1200

    Added variables

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
-echo "Hello World!";
+echo "Hello World!"; // write out "Hello, World" to the browser
+// hello 
+// world
+
+/**
+ * lalaal
+ * lalalala
+ */
+
+$name = "John Doe"; // variable to store name
+$age = 30; // variable to store age
+echo $age;
+$age = $age * 2 * 3 * 4 * 5 * 6 * 7 * 8 * 9 * 10; // multiply age by 2, then by 3, then by 4, then by 5, then by 6, then by 7, then by 8, then by 9, then by 10
+echo $age; // output the updated age
+
+echo 2+2; // output the result of 2 + 2
 ?>
