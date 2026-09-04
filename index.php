ced8d75228c51c462863a437e1797c3c7c665bd0
laura Bachverk <laura.Bachverk@gmail.com>
2026-09-03 23:07:07 -1200

    Add array examples

--- index.php
+++ index.php
@@ -1,20 +1,27 @@
 <?php
-$test = true;
-$test = false;
-$test = true && true;
-$test = true || false;
-$test = !false;
-$test = 1 > 5;
-$test = 10 < 5;
-$test = 10 < 10;
-$test = 10 == 10;
-$test = 10 >= 10;
-$test = 10 <= 10;
-$test = 10 != 5;
-$test = 10 !== 5;
-$test = 10 == '10';
-$test = 10 === '10';
-$test = true && false || (true && true);
+$test = [1, 2, 'world', true];
+var_dump($test[2]);
+$test[2] = 'lalal';
 var_dump($test);
-
-
+$test[4] = 'new value';
+var_dump($test);
+$test[]= 'another value';
+var_dump($test);
+array_push($test, 'pushed value');
+var_dump($test);
+$test = [     'name'=> 'Laura' , 
+              'age' => 30, 
+              1,
+              3,
+              'vdvdvd',
+              100 => 'new value',
+              5,
+        ];
+var_dump($test['name']);
+var_dump($test);
+$test = [
+    [1, 2, 3,],
+    [4, 5, 6,],
+    [7, 8, 9,],
+];
+var_dump($test[1][2]);
