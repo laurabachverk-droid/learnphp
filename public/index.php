ed4ac3d9c2130097b26fd2ebfd9edeaca2e9b7ca
Laura Bachverk <Laura.bachverk@gmail.com>
2026-09-21 00:14:01 -1200

    added forms page again fix

---
+++ public/index.php
@@ -0,0 +1,33 @@
+<?php
+use App\Router;
+
+if (preg_match('/\.(?:png|jpg|jpeg|gif|css|js)$/', $_SERVER["REQUEST_URI"])) {
+    return false;    // serve the requested resource as-is.
+}
+
+
+spl_autoload_register(function ($class){
+    $class = substr($class,4);
+    $class = str_replace('\\', '/', $class);
+    require_once __DIR__ . "/../src/$class.php";
+});
+
+require_once __DIR__ . '/../src/Router.php';
+require __DIR__ . '/../helpers.php';
+require __DIR__ . '/../routes.php';
+
+$routerClass = 'App\\Router';
+$router = new $routerClass($_SERVER['REQUEST_URI']);
+$match = $router->match();
+if($match) {
+    if(is_callable($match['action'])) {
+        call_user_func($match['action']);
+    } else if(is_array($match['action'])) {
+        $class = $match['action'][0];
+        $controller = new $class();
+        $method = $match['action'][1];
+        $controller->$method();
+    }
+} else {
+    echo 404;
+}
